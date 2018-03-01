92e765619ca3693f717ebdc956d3e578678dee55
Daniel <Daniel@CSistemas04.une.net.co>
2018-03-01 11:53:58 -0500

    auditoria 1.7.1

--- InventarioSilvestres/src/dao/temperatura/jdbc/TemperaturaSQL.java
+++ InventarioSilvestres/src/dao/temperatura/jdbc/TemperaturaSQL.java
@@ -6,7 +6,7 @@ public class TemperaturaSQL {
 	public static final String FIND_BY_ID ="SELECT id_temperatura, valor_temperatura, fecha_temperatura, sector_temperatura, lugar_temperatura, fk_id_bloque, fk_id_persona, nombre_persona  FROM "+ TABLE_NAME+" INNER JOIN persona on persona.id_persona = "+TABLE_NAME+".fk_id_persona where id_temperatura = ? ";
 	public static final String INSERT="INSERT INTO "+TABLE_NAME+" ( valor_temperatura, fecha_temperatura, sector_temperatura, lugar_temperatura, fk_id_bloque,  fk_id_persona)  VALUES ( ?, ?, ?, ?, ?, ?)";
 	public static final String UPDATE ="UPDATE "+TABLE_NAME+"  SET  valor_temperatura = ?, fecha_temperatura = ?, sector_temperatura = ?, lugar_temperatura = ?, fk_id_bloque = ?, fk_id_persona = ?  where id_temperatura = ?";
-	public static final String DELETE ="DELETE FROM "+ TABLE_NAME+" where id_temperatura = ?";
+	public static final String DELETE ="UPDATE "+TABLE_NAME+"  SET  activo_temperatura = 'N' WHERE id_temperatura = ?";
 	public static final String LIST = "SELECT id_temperatura, valor_temperatura, fecha_temperatura, sector_temperatura, lugar_temperatura, fk_id_bloque, fk_id_persona, nombre_persona  FROM "+TABLE_NAME+" INNER JOIN persona on persona.id_persona = "+TABLE_NAME+".fk_id_persona";
 	
 
