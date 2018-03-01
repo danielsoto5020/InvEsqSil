4a156c4f9e62b4d5b75099592a1ad9d8408bf73a
Daniel <Daniel@CSistemas04.une.net.co>
2018-03-01 14:22:03 -0500

    movimientos 1.3.0

--- InventarioSilvestres/src/dao/siembra/jdbc/SiembraSQL.java
+++ InventarioSilvestres/src/dao/siembra/jdbc/SiembraSQL.java
@@ -3,12 +3,14 @@ package dao.siembra.jdbc;
 public class SiembraSQL {
 	
 	private static final String TABLE_NAME="siembra";
-	public static final String FIND_BY_ID ="SELECT id_siembra, cantidad_siembra, fecha_siembra, observacion_siembra, fk_id_cama, fk_id_variedad, fk_id_empleado  FROM "+TABLE_NAME+" where id_siembra = ?";
-	public static final String INSERT="INSERT INTO "+TABLE_NAME+" ( cantidad_siembra, fecha_siembra, observacion_siembra, fk_id_cama, fk_id_variedad, fk_id_empleado)  VALUES ( ?, ?, ?, ?, ?, ?)";
-	public static final String UPDATE ="UPDATE "+TABLE_NAME+"  SET  cantidad_siembra = ?, fecha_siembra = ?, observacion_siembra = ?, fk_id_cama = ?, fk_id_variedad = ?, fk_id_empleado = ?  where id_siembra = ?";
+	public static final String FIND_BY_ID ="SELECT id_siembra, cantidad_siembra, fecha_siembra, observacion_siembra, fk_id_variedad, fk_id_empleado, fk_id_cama  FROM "+TABLE_NAME+" where id_siembra = ?";
+	public static final String INSERT="INSERT INTO "+TABLE_NAME+" ( cantidad_siembra, fecha_siembra, observacion_siembra, fk_id_variedad, fk_id_empleado, fk_id_cama)  VALUES ( ?, ?, ?, ?, ?, ?)";
+	public static final String UPDATE ="UPDATE "+TABLE_NAME+"  SET  cantidad_siembra = ?, fecha_siembra = ?, observacion_siembra = ?, fk_id_variedad = ?, fk_id_empleado = ?, fk_id_cama = ?  where id_siembra = ?";
 	public static final String DELETE ="DELETE FROM "+ TABLE_NAME+" where id_siembra = ?";
-	public static final String LIST = "SELECT id_siembra, cantidad_siembra, fecha_siembra, observacion_siembra, fk_id_cama, fk_id_variedad, fk_id_empleado FROM "+TABLE_NAME;
+	public static final String LIST = "SELECT id_siembra, cantidad_siembra, fecha_siembra, observacion_siembra, fk_id_variedad, fk_id_empleado, fk_id_cama FROM "+TABLE_NAME;
 	
-
-
+	
+	public SiembraSQL() {
+		super();
+	}
 }
--- InventarioSilvestres/src/dto/IngresoDTO.java
+++ InventarioSilvestres/src/dto/IngresoDTO.java
@@ -1,5 +1,91 @@
 package dto;
 
 public class IngresoDTO {
+	
+	private Integer id;
+	private String cantidad;
+	private String puesto;
+	private String lado;
+	private String fecha;
+	private String variedad;
+	private String empleado;
+	private String cama;
+	private String siembra;
+	
+	public IngresoDTO() {
+		super();
+	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(String cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public String getPuesto() {
+		return puesto;
+	}
+
+	public void setPuesto(String puesto) {
+		this.puesto = puesto;
+	}
+
+	public String getLado() {
+		return lado;
+	}
+
+	public void setLado(String lado) {
+		this.lado = lado;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getVariedad() {
+		return variedad;
+	}
+
+	public void setVariedad(String variedad) {
+		this.variedad = variedad;
+	}
+
+	public String getEmpleado() {
+		return empleado;
+	}
+
+	public void setEmpleado(String empleado) {
+		this.empleado = empleado;
+	}
+
+	public String getCama() {
+		return cama;
+	}
+
+	public void setCama(String cama) {
+		this.cama = cama;
+	}
+
+	public String getSiembra() {
+		return siembra;
+	}
+
+	public void setSiembra(String siembra) {
+		this.siembra = siembra;
+	}
+	
 }
